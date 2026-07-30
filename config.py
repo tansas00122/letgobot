afb438e79df7d7a3d30e3dc4f7a752f3abeda5a3
tansas00122 <120738705+tansas00122@users.noreply.github.com>
2026-07-30 19:34:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SEARCH_URL = "https://www.letgo.com/arama?query_text=iphone%2013&isSearchCall=true"
+SEARCH_URL = "https://www.letgo.com/arama?query_text=Logitech%20G300s&isSearchCall=true"
 WEBHOOK_URL = "https://hook.eu1.make.com/ut8wg3gg8dder9ikvd7d4y6t6um2kiao"
 CHECK_INTERVAL_SECONDS = 15
 PAGE_LOAD_TIMEOUT = 30000
